a765a54b8e34fb444f3e00fae6f3ee23e2f7077a
David Segun <segundavidk@gmail.com>
2022-10-03 16:06:23 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's Begin</h1>
+      <NavBar>Hi</NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  './NavBar.css'
+
+function NavBar() {
+
+    return(
+        <div className="navbar">
+            <h1>RealEase</h1>
+        </div>
+    )
+
+}
+
+export default NavBar;
